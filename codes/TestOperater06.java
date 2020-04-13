724beb17a15e5f70567033921b4a9805e3b7cdc8
Don <d_chn@outlook.com>
2020-04-14 00:21:58 +0800

    这是第一次版本提交

---
+++ codes/TestOperater06.java
@@ -0,0 +1,7 @@
+public class TestOperater06 {
+	public static void main(String[] args) {
+		int score = 90;
+		String a = score>60?"及格":"不及格";
+		System.out.println(a);
+	}
+}
---
+++ codes/TestOperater07.java
@@ -0,0 +1,7 @@
+public class TestOperater07 {
+	public static void main(String[] args) {
+		//测试运算符优先级
+		boolean s1=true,s2=true,s3=false;
+		System.out.println(s1||s2&&s3);//与的优先级高于或
+	}
+}
